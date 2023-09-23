91b8ed90ea5ca02d12d97be410edf738a915d273
KaeserOfHonour <103267805+KaeserOfHonour@users.noreply.github.com>
2023-09-23 18:35:19 +0300

    Added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+        <footer className="mt-8 text-center">
+            <span className="text-sm text-zinc-500">© 2023 Kyrylo Tymchyshyn. All Rights Reserved.</span>
+        </footer>
+    );
+};
+export default Footer;
