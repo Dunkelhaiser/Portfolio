76b0a60092702633fb16bd9d9e5c977fc1d36054
KaeserOfHonour <103267805+KaeserOfHonour@users.noreply.github.com>
2023-09-23 14:54:54 +0300

    Added skill icon sizes

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -10,7 +10,7 @@ const Skill = ({ skill }: Props) => {
     return (
         <Button variant="secondary" asChild key={skill.name}>
             <a href={skill.link} target="_blank" rel="noopener noreferrer" aria-label={skill.name}>
-                <Image src={skill.icon} alt={`${skill.name} Logo`} width={50} height={50} />
+                <Image src={skill.icon} alt={`${skill.name} Logo`} width={50} height={50} className="h-[50px] w-[50px]" />
             </a>
         </Button>
     );
