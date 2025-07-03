d95cf26b3cb6e39a050dbbf80be58a8e99954ddd
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-07-03 13:30:38 +0300

    update about section

--- src/components/Sections/About.tsx
+++ src/components/Sections/About.tsx
@@ -4,10 +4,10 @@ const About = () => {
     return (
         <Section id="about" heading="About Me" className="sm:mt-0" threshold={1}>
             <p className="mt-3 max-w-prose text-center text-lg text-zinc-700">
-                My name is Kyrylo and I&apos;m 19 years old. I am a computer science student at Ivan Franko National University of Lviv. I
-                became interested in programming at the age of 12. My first program was written in C++, but I found my passion in web
-                development, as I can make programs that anyone can access without any pain. I&apos;m positioning myself as a full-stack
-                developer with my core stack being React/Next.js, Node.js, TypeScript, and PostgreSQL.
+                My name is Kyrylo and I&apos;m 21 years old. I hold a Bachelor&apos;s degree in Computer Science from the Ivan Franko
+                National University of Lviv. I became interested in programming at the age of 12. My first program was written in C++, but I
+                found my passion in web development, as I can make programs that anyone can access without any pain. I&apos;m positioning
+                myself as a full-stack developer with my core stack being React/Next.js, Node.js, TypeScript, and PostgreSQL.
             </p>
         </Section>
     );
